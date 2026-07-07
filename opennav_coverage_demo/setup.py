0f845d143711b4548c570512d29ee0868da42ed6
Joey Quinlan <jquinlan@arosystems.ai>
2026-07-07 10:19:22 -0600

    0.0.2

--- opennav_coverage_demo/setup.py
+++ opennav_coverage_demo/setup.py
@@ -8,7 +8,7 @@ package_name = 'opennav_coverage_demo'
 
 setup(
     name=package_name,
-    version='1.0.0',
+    version='0.0.2',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
